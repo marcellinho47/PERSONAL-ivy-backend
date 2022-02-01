1f128754e0427312d02878dede296559952bca72
Marcello Alves <marcellodoalves@hotmail.com>
2022-01-31 23:02:39 -0300

    Iniciando projeto + scripts banco de dados

---
+++ src/main/java/store/ivy/sisivy/SisIvyApplication.java
@@ -0,0 +1,12 @@
+package store.ivy.sisivy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisIvyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisIvyApplication.class, args);
+	}
+}
---
+++ src/test/java/store/ivy/sisivy/SisIvyApplicationTests.java
@@ -0,0 +1,13 @@
+package store.ivy.sisivy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisIvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
